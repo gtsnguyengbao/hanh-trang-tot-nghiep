8e35eb60d4dadfb24a0e0d386b159004cd186587
Nguyen Gia Bao <nguyengiabao21062007@gmail.com>
2025-03-08 23:14:27 +0700

    a new website

---
+++ assetes/js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('loginForm');
+    const errorMessage = document.getElementById('errorMessage');
+
+    // Danh sách tài khoản mẫu
+    const validUsers = {
+        'nguyengbao': '@Nguyenbao_2106',
+        'mainhatbaoan': '17102006',
+    };
+
+    if (loginForm) {
+        loginForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            const username = document.getElementById('username').value;
+            const password = document.getElementById('password').value;
+
+            if (validUsers[username] && validUsers[username] === password) {
+                // Đăng nhập thành công → Chuyển hướng sang trang chính
+                window.location.href = 'home.html';
+            } else {
+                errorMessage.textContent = 'Tên người dùng hoặc mật khẩu không đúng!';
+            }
+        });
+    }
+});
